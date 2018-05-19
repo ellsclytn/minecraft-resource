3778d16c7083cc62e7eee69f8c265b3026138c1b
Ellis Clayton <ellis@ellisclayton.info>
2018-05-19 16:21:18 +1000

    Add item definitions

--- src/schema.ts
+++ src/schema.ts
@@ -27,6 +27,147 @@ export default {
     'redstone': 1
   },
   'enrichedAlloy': {
+    'redstone': 1,
     'ironIngot': 1
+  },
+  'transferNodeItems': {
+    'transferPipe': 1,
+    'redstoneBlock': 1,
+    'redstone': 2,
+    'stone': 2,
+    'woodenChest': 1
+  },
+  'transferPipe': {
+    'stoneSlab': 6,
+    'glass': 2,
+    'redstone': 1
+  },
+  'redstoneBlock': {
+    'redstone': 9
+  },
+  'woodenChest': {
+    'woodPlank': 8
+  },
+  'itemFilter': {
+    'redstone': 4,
+    'stick': 4,
+    'string': 1
+  },
+  'ultimateEnergyCube': {
+    'atomicAlloy': 4,
+    'diamond': 2,
+    'energyTablet': 2,
+    'eliteEnergyCube': 1
+  },
+  'atomicAlloy': {
+    'reinforcedAlloy': 1,
+    'obsidian': 1,
+    'diamond': 1
+  },
+  'reinforcedAlloy': {
+    'diamond': 1,
+    'enrichedAlloy': 1
+  },
+  'energyTablet': {
+    'goldIngot': 3,
+    'redstone': 4,
+    'enrichedAlloy': 2
+  },
+  'eliteEnergyCube': {
+    'reinforcedAlloy': 4,
+    'goldIngot': 2,
+    'energyTablet': 2,
+    'advancedEnergyCube': 1
+  },
+  'advancedEnergyCube': {
+    'enrichedAlloy': 4,
+    'energyTablet': 2,
+    'osmiumIngot': 2,
+    'basicEnergyCube': 1
+  },
+  'tier3HeavyDutyPlate': {
+    'tier2HeavyDutyPlate': 1,
+    'compressedDesh': 1
+  },
+  'tier2HeavyDutyPlate': {
+    'heavyDutyPlating': 1,
+    'compressedMeteoricIron': 1
+  },
+  'heavyDutyPlating': {
+    'compressedSteel': 1,
+    'compressedAluminum': 1,
+    'compressedBronze': 1
+  },
+  'compressedSteel': {
+    'compressedIron': 1,
+    'coal': 2
+  },
+  'compressedAluminum': {
+    'aluminumIngot': 2
+  },
+  'compressedBronze': {
+    'compressedTin': 1,
+    'compressedCopper': 1
+  },
+  'compressedTin': {
+    'tinIngot': 2
+  },
+  'compressedCopper': {
+    'copperIngot': 2
+  },
+  'heavyNoseCone': {
+    'redstoneTorch': 1,
+    'tier3HeavyDutyPlate': 3
+  },
+  'redstoneTorch': {
+    'stick': 1,
+    'redstone': 1
+  },
+  'stick': {
+    'wood': 0.25
+  },
+  'heavyRocketEngine': {
+    'tier3HeavyDutyPlate': 4,
+    'flintAndSteel': 1,
+    'stoneButton': 1,
+    'tinCanister': 1,
+    'airVent': 1
+  },
+  'flintAndSteel': {
+    'flint': 1,
+    'ironIngot': 1
+  },
+  'tinCanister': {
+    'tinIngot': 3.5
+  },
+  'stoneButton': {
+    'stone': 1
+  },
+  'airVent': {
+    'compressedTin': 3,
+    'compressedSteel': 1
+  },
+  'heavyRocketFin': {
+    'tier3HeavyDutyPlate': 4,
+    'tier2HeavyDutyPlate': 2
+  },
+  'tier1Booster': {
+    'compressedMeteoricIron': 4,
+    'airVent': 1,
+    'fuelCanister': 1,
+    'heavyDutyPlating': 2
+  },
+  'fuelCanister': {
+    'compressedTin': 6,
+    'glass': 1,
+    'compressedSteel': 1,
+    'tinCanister': 1
+  },
+  'tier3Rocket': {
+    'tier3HeavyDutyPlate': 10,
+    'heavyNoseCone': 1,
+    'heavyRocketEngine': 1,
+    'heavyRocketFin': 4,
+    'tier1Booster': 2
   }
 }
