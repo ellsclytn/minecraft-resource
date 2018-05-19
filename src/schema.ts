37370a7cfdb180c80637f7b1f40dd9f3921515cd
Ellis Clayton <ellis@ellisclayton.info>
2018-05-19 21:12:13 +1000

    Add item definitions

--- src/schema.ts
+++ src/schema.ts
@@ -1,34 +1,224 @@
 export default {
-  'kineticGearboxRotor': {
-    'carbonRotorBlade': 4,
-    'shaftRefinedIron': 1
-  },
-  'carbonRotorBlade': {
-    'rawCarbonMesh': 9
-  },
-  'rawCarbonMesh': {
-    'rawCarbonFibre': 2
-  },
-  'rawCarbonFibre': {
-    'coalDust': 4
-  },
-  'shaftRefinedIron': {
-    'blockOfSteel': 1
-  },
-  'blockOfSteel': {
-    'steelIngot': 9
+  'advancedEnergyCube': {
+    'enrichedAlloy': 4,
+    'energyTablet': 2,
+    'osmiumIngot': 2,
+    'basicEnergyCube': 1
   },
   'advancedUniversalCable': {
     'basicUniversalCable': 8,
     'enrichedAlloy': 1
   },
+  'airLockController': {
+    'compressedSteel': 6,
+    'compressedMeteoricIron': 2,
+    'basicWafer': 1
+  },
+  'airLockFrame': {
+    'compressedAluminum': 6 / 4,
+    'compressedSteel': 2 / 4,
+    'oxygenConcentrator': 1 / 4
+  },
+  'airVent': {
+    'compressedTin': 3,
+    'compressedSteel': 1
+  },
+  'atomicAlloy': {
+    'reinforcedAlloy': 1,
+    'obsidian': 1,
+    'diamond': 1
+  },
   'basicUniversalCable': {
     'steelIngot': 2,
     'redstone': 1
   },
+  'basicWafer': {
+    'diamond': 1 / 3,
+    'silicon': 2 / 3,
+    'redstone': 1 / 3,
+    'redstoneTorch': 1 / 3
+  },
+  'bed': {
+    'wool': 3,
+    'woodPlank': 3
+  },
+  'blockOfSteel': {
+    'steelIngot': 9
+  },
+  'carbonRotorBlade': {
+    'rawCarbonMesh': 9
+  },
+  'compressedAluminum': {
+    'aluminumIngot': 2
+  },
+  'compressedBronze': {
+    'compressedTin': 1,
+    'compressedCopper': 1
+  },
+  'compressedCopper': {
+    'copperIngot': 2
+  },
+  'compressedSteel': {
+    'compressedIron': 1,
+    'coal': 2
+  },
+  'compressedTin': {
+    'tinIngot': 2
+  },
+  'cryogenicChamber': {
+    'compressedDesh': 6,
+    'heavyDutyPlating': 2,
+    'bed': 1
+  },
+  'eliteEnergyCube': {
+    'reinforcedAlloy': 4,
+    'goldIngot': 2,
+    'energyTablet': 2,
+    'advancedEnergyCube': 1
+  },
+  'energyTablet': {
+    'goldIngot': 3,
+    'redstone': 4,
+    'enrichedAlloy': 2
+  },
   'enrichedAlloy': {
     'redstone': 1,
     'ironIngot': 1
+  },
+  'flintAndSteel': {
+    'flint': 1,
+    'ironIngot': 1
+  },
+  'fuelCanister': {
+    'compressedTin': 6,
+    'glass': 1,
+    'compressedSteel': 1,
+    'tinCanister': 1
+  },
+  'heavyDutyPlating': {
+    'compressedSteel': 1,
+    'compressedAluminum': 1,
+    'compressedBronze': 1
+  },
+  'heavyNoseCone': {
+    'redstoneTorch': 1,
+    'tier3HeavyDutyPlate': 3
+  },
+  'heavyRocketEngine': {
+    'tier3HeavyDutyPlate': 4,
+    'flintAndSteel': 1,
+    'stoneButton': 1,
+    'tinCanister': 1,
+    'airVent': 1
+  },
+  'heavyRocketFin': {
+    'tier3HeavyDutyPlate': 4,
+    'tier2HeavyDutyPlate': 2
+  },
+  'itemFilter': {
+    'redstone': 4,
+    'stick': 4,
+    'string': 1
+  },
+  'kineticGearboxRotor': {
+    'carbonRotorBlade': 4,
+    'shaftRefinedIron': 1
+  },
+  'oxygenBubbleDistributor': {
+    'compressedSteel': 4,
+    'airVent': 2,
+    'compressedAluminum': 1,
+    'oxygenFan': 2
+  },
+  'oxygenCollector': {
+    'compressedSteel': 3,
+    'oxygenFan': 1,
+    'airVent': 1,
+    'tinCanister': 1,
+    'compressedAluminum': 2,
+    'oxygenConcentrator': 1
+  },
+  'oxygenCompressor': {
+    'compressedSteel': 4,
+    'compressedAluminum': 3,
+    'compressedBronze': 1,
+    'oxygenConcentrator': 1
+  },
+  'oxygenConcentrator': {
+    'compressedSteel': 4,
+    'compressedTin': 3,
+    'tinCanister': 1,
+    'airVent': 1
+  },
+  'oxygenFan': {
+    'compressedSteel': 4,
+    'redstone': 1,
+    'basicWafer': 1
+  },
+  'oxygenSealer': {
+    'compressedAluminum': 4,
+    'compressedSteel': 2,
+    'oxygenFan': 1,
+    'airVent': 2
+  },
+  'rawCarbonFibre': {
+    'coalDust': 4
+  },
+  'rawCarbonMesh': {
+    'rawCarbonFibre': 2
+  },
+  'redstoneBlock': {
+    'redstone': 9
+  },
+  'redstoneTorch': {
+    'stick': 1,
+    'redstone': 1
+  },
+  'reinforcedAlloy': {
+    'diamond': 1,
+    'enrichedAlloy': 1
+  },
+  'shaftRefinedIron': {
+    'blockOfSteel': 1
+  },
+  'spacePack': {
+    'oxygenCollector': 1,
+    'oxygenCompressor': 1,
+    'oxygenSealer': 2,
+    'airLockFrame': 10,
+    'airLockController': 2,
+    'cryogenicChamber': 2,
+    'oxygenBubbleDistributor': 1
+  },
+  'stick': {
+    'wood': 0.25
+  },
+  'stoneButton': {
+    'stone': 1
+  },
+  'tier1Booster': {
+    'compressedMeteoricIron': 4,
+    'airVent': 1,
+    'fuelCanister': 1,
+    'heavyDutyPlating': 2
+  },
+  'tier2HeavyDutyPlate': {
+    'heavyDutyPlating': 1,
+    'compressedMeteoricIron': 1
+  },
+  'tier3HeavyDutyPlate': {
+    'tier2HeavyDutyPlate': 1,
+    'compressedDesh': 1
+  },
+  'tier3Rocket': {
+    'tier3HeavyDutyPlate': 10,
+    'heavyNoseCone': 1,
+    'heavyRocketEngine': 1,
+    'heavyRocketFin': 4,
+    'tier1Booster': 2
+  },
+  'tinCanister': {
+    'tinIngot': 3.5
   },
   'transferNodeItems': {
     'transferPipe': 1,
@@ -42,132 +232,13 @@ export default {
     'glass': 2,
     'redstone': 1
   },
-  'redstoneBlock': {
-    'redstone': 9
-  },
-  'woodenChest': {
-    'woodPlank': 8
-  },
-  'itemFilter': {
-    'redstone': 4,
-    'stick': 4,
-    'string': 1
-  },
   'ultimateEnergyCube': {
     'atomicAlloy': 4,
     'diamond': 2,
     'energyTablet': 2,
     'eliteEnergyCube': 1
   },
-  'atomicAlloy': {
-    'reinforcedAlloy': 1,
-    'obsidian': 1,
-    'diamond': 1
-  },
-  'reinforcedAlloy': {
-    'diamond': 1,
-    'enrichedAlloy': 1
-  },
-  'energyTablet': {
-    'goldIngot': 3,
-    'redstone': 4,
-    'enrichedAlloy': 2
-  },
-  'eliteEnergyCube': {
-    'reinforcedAlloy': 4,
-    'goldIngot': 2,
-    'energyTablet': 2,
-    'advancedEnergyCube': 1
-  },
-  'advancedEnergyCube': {
-    'enrichedAlloy': 4,
-    'energyTablet': 2,
-    'osmiumIngot': 2,
-    'basicEnergyCube': 1
-  },
-  'tier3HeavyDutyPlate': {
-    'tier2HeavyDutyPlate': 1,
-    'compressedDesh': 1
-  },
-  'tier2HeavyDutyPlate': {
-    'heavyDutyPlating': 1,
-    'compressedMeteoricIron': 1
-  },
-  'heavyDutyPlating': {
-    'compressedSteel': 1,
-    'compressedAluminum': 1,
-    'compressedBronze': 1
-  },
-  'compressedSteel': {
-    'compressedIron': 1,
-    'coal': 2
-  },
-  'compressedAluminum': {
-    'aluminumIngot': 2
-  },
-  'compressedBronze': {
-    'compressedTin': 1,
-    'compressedCopper': 1
-  },
-  'compressedTin': {
-    'tinIngot': 2
-  },
-  'compressedCopper': {
-    'copperIngot': 2
-  },
-  'heavyNoseCone': {
-    'redstoneTorch': 1,
-    'tier3HeavyDutyPlate': 3
-  },
-  'redstoneTorch': {
-    'stick': 1,
-    'redstone': 1
-  },
-  'stick': {
-    'wood': 0.25
-  },
-  'heavyRocketEngine': {
-    'tier3HeavyDutyPlate': 4,
-    'flintAndSteel': 1,
-    'stoneButton': 1,
-    'tinCanister': 1,
-    'airVent': 1
-  },
-  'flintAndSteel': {
-    'flint': 1,
-    'ironIngot': 1
-  },
-  'tinCanister': {
-    'tinIngot': 3.5
-  },
-  'stoneButton': {
-    'stone': 1
-  },
-  'airVent': {
-    'compressedTin': 3,
-    'compressedSteel': 1
-  },
-  'heavyRocketFin': {
-    'tier3HeavyDutyPlate': 4,
-    'tier2HeavyDutyPlate': 2
-  },
-  'tier1Booster': {
-    'compressedMeteoricIron': 4,
-    'airVent': 1,
-    'fuelCanister': 1,
-    'heavyDutyPlating': 2
-  },
-  'fuelCanister': {
-    'compressedTin': 6,
-    'glass': 1,
-    'compressedSteel': 1,
-    'tinCanister': 1
-  },
-  'tier3Rocket': {
-    'tier3HeavyDutyPlate': 10,
-    'heavyNoseCone': 1,
-    'heavyRocketEngine': 1,
-    'heavyRocketFin': 4,
-    'tier1Booster': 2
+  'woodenChest': {
+    'woodPlank': 8
   }
 }
